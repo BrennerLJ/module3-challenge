df7e41b299851bcc3845c7f9021f7a359d8d7194
Brenner Jolley <bjbrenardjoey@gmail.com>
2024-01-17 23:55:45 -0700

    Trying to finish

--- script.js
+++ script.js
@@ -1,51 +1,74 @@
 // Assignment code here
 // Declaring variables to use throughout the function.
 var keyLength;
-var uppercase = confirm("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
-var lowercase = confirm("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-var symbols = confirm("!", "?", "@", "#", "$", "%", "^", "&", "*", "+", "=", "/", "~");
-var numbers = confirm("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+var confirmUppercase;
+var confirmLowercase;
+var confirmSymbols;
+var confirmNumbers;
+
+//All these values are used to scramble a random password.
+uppercase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+lowercase = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+symbols = ("!", "?", "@", "#", "$", "%", "^", "&", "*", "+", "=", "/", "~");
+numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+
 
 var options;
-
+//This function takes the user input for number of characters desired in their password.
 function generatePassword() {
   keyLength = prompt("How long would you like your password? Choose between 8 and 128 characters.")
 
   if (!keyLength) {
     alert("You must input a value.");
     } else if (keyLength < 8 || keyLength > 128) {
-      prompt("Choose a value between 8 and 128.");
+      prompt("You must choose a value between 8 and 128.");
     } else {
+      //If the above is true, the prompt will ask what characters they want their password to contain.
       confirmUppercase = confirm("Will your password contain uppercase letters?");
       confirmLowercase = confirm("Will your password contain lowercase letters?");
       confirmSymbols = confirm("Will your password contain symbols?");
       confirmNumbers = confirm("Will your password contain numbers?");
     };
 
+var get = document.querySelector("#generate");
+
 get.addEventListener("click", function () {
   ps = generatePassword();
   document.getElementById("password").placeholder = ps;
 });
+
   //This if statement displays an alert if you don't select any criteria.
   if (!uppercase && !lowercase && !symbols && !numbers) {
     options = alert("You must select an option.");
   }
-  else if (uppercase && lowercase && symbols && numbers) {
-    keyLength = alert("Congratulations!")
-  }
-  //These else if statements take your selected options and display something depending on your selections.
   
+  //These else if statements take your selected options and display something depending on your selections.
 
-  var finalPassword = ""
+  else if (uppercase && lowercase && symbols && numbers) {
+
+  }
+
+
+
+
+  //This is supposed to display the result in the text box, but I couldn't figure it out. Yet...
+  var displayPassword = ""
   for (var i = 0; i < keyLength; i++) {
     var rng = [Math.floor(Math.random() * multiSelect.length)];
-    finalPassword = finalPassword + multiSelect[rng];
+    displayPassword = displayPassword + multiSelect[rng];
   }
-  return finalPassword;
-  var ps = password.join("");
+  return displayPassword;
+  
+  var ps = password.join(""); 
   UserInput(ps);
   return ps;
+  
+  function UserInput(ps) {
+   document.getElementById("password").textContent = ps;
+  }
   };
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
