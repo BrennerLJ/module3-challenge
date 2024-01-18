938e0cfc92b830974a68f200c36fec1fe541f3c6
Brenner Jolley <bjbrenardjoey@gmail.com>
2024-01-17 22:41:28 -0700

    creating if else statements to take the users input

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 // Assignment code here
-  var keyLength;
-  var uppercase = confirm("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
-  var lowercase = confirm("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-  var symbols = confirm("!", "?", "@", "#", "$", "%", "^", "&", "*", "+", "=", "/", "~");
-  var numbers = confirm("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+// Declaring variables to use throughout the function.
+var keyLength;
+var uppercase = confirm("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+var lowercase = confirm("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+var symbols = confirm("!", "?", "@", "#", "$", "%", "^", "&", "*", "+", "=", "/", "~");
+var numbers = confirm("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
 
-  function generatePassword() {
-    keyLength = prompt("How long would you like your password? Choose between 8 and 128 characters.")
+var options;
 
-    if (!keyLength) {
-      alert("You must input a value.");
+function generatePassword() {
+  keyLength = prompt("How long would you like your password? Choose between 8 and 128 characters.")
+
+  if (!keyLength) {
+    alert("You must input a value.");
     } else if (keyLength < 8 || keyLength > 128) {
       prompt("Choose a value between 8 and 128.");
     } else {
@@ -17,17 +20,34 @@
       confirmLowercase = confirm("Will your password contain lowercase letters?");
       confirmSymbols = confirm("Will your password contain symbols?");
       confirmNumbers = confirm("Will your password contain numbers?");
-    }
+    };
 
-  let finalPassword = ""
-  for (let i = 0; i < keyLength; i++) {
-    let rng = [Math.floor(Math.random() * multiSelect.length)];
+get.addEventListener("click", function () {
+  ps = generatePassword();
+  document.getElementById("password").placeholder = ps;
+});
+  //This if statement displays an alert if you don't select any criteria.
+  if (!uppercase && !lowercase && !symbols && !numbers) {
+    options = alert("You must select an option.");
+  }
+  else if (uppercase && lowercase && symbols && numbers) {
+    keyLength = alert("Congratulations!")
+  }
+  //These else if statements take your selected options and display something depending on your selections.
+  
+
+  var finalPassword = ""
+  for (var i = 0; i < keyLength; i++) {
+    var rng = [Math.floor(Math.random() * multiSelect.length)];
     finalPassword = finalPassword + multiSelect[rng];
   }
   return finalPassword;
+  var ps = password.join("");
+  UserInput(ps);
+  return ps;
   };
 
-  // Get references to the #generate element
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
